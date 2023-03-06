a0d28a4e2e95ae0b922e452792f802e840d38aea
Luccas  Silva <luccassilvadev@gmail.com>
2023-03-06 01:53:28 -0300

    Create Temperatura.java
    Conversão de Fahrenheit em Celsius

---
+++ exercicios/src/fundamentos/Temperatura.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Temperatura {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite a temperatura em °F : ");
+        double Fahrenheit = entrada.nextDouble();
+        double Celsius = (Fahrenheit - 32) * 5.0 / 9.0;
+        System.out.println("A temperatura em °C é " + Celsius);
+    }
+}
