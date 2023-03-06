e5881b80d0a33e5ec310137e8d3744e4d89bd98d
Luccas  Silva <luccassilvadev@gmail.com>
2023-03-06 01:41:59 -0300

    Create AreaCircunferencia.java

---
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        double raio = 5.0;
+        double area = 3.14159 * Math.pow(raio, 2);
+        System.out.println(area);
+        System.out.println(Math.PI);
+
+    }
+}
