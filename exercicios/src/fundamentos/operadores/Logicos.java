b1ad55774f3f1d689af318c22c1c93b3952b50a5
Luccas  Silva <luccassilvadev@gmail.com>
2023-03-06 18:34:43 -0300

    Create Logicos.java

---
+++ exercicios/src/fundamentos/operadores/Logicos.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        // Tabela Verdade E
+        System.out.println("\nTabela Verdade E And)");
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+        // Tabela Verdade OU
+        System.out.println("\nTabela Verdade OU (Or)");
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+        // Tabela Verdade OU Exclusivo
+        System.out.println("\nTabela Verdade OU Exclusivo (xor)");
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ true);
+        System.out.println(false ^ false);
+
+    }
+
+}
