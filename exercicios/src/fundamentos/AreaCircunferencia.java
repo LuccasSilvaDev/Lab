e4e9602934fd95166f7c7dfebbf50eb202235394
Luccas  Silva <luccassilvadev@gmail.com>
2023-03-06 19:18:19 -0300

    Update AreaCircunferencia.java

--- exercicios/src/fundamentos/AreaCircunferencia.java
+++ exercicios/src/fundamentos/AreaCircunferencia.java
@@ -1,10 +1,16 @@
 package fundamentos;
 
+import java.util.Scanner;
+
 public class AreaCircunferencia {
     public static void main(String[] args) {
-        double raio = 5.0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o raio: ");
+        double raio = entrada.nextDouble();
+        // double raio = 5.0;
         double area = 3.14159 * Math.pow(raio, 2);
         System.out.println(area);
         System.out.println(Math.PI);
+        entrada.close();
     }
 }
