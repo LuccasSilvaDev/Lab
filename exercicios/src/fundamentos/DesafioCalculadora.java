c5123b7112d9d55d670cd05729682434fbe24ad2
Luccas  Silva <luccassilvadev@gmail.com>
2023-03-07 00:41:14 -0300

    Create DesafioCalculadora.java

---
+++ exercicios/src/fundamentos/DesafioCalculadora.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Digite um valor: ");
+        double valor = Entrada.nextDouble();
+
+        System.out.println("Digite um operador: ");
+        String operador = Entrada.next();
+
+        System.out.println("Digite outro valor: ");
+        double outroValor = Entrada.nextDouble();
+
+        double resultado = "+".equals(operador) ? valor + outroValor : 0;
+        resultado = "-".equals(operador) ? valor - outroValor : resultado;
+        resultado = "*".equals(operador) ? valor * outroValor : resultado;
+        resultado = "/".equals(operador) ? valor / outroValor : resultado;
+        resultado = "%".equals(operador) ? valor % outroValor : resultado;
+
+        System.out.println(resultado);
+        Entrada.close();
+    }
+}
